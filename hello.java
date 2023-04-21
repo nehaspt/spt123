d4c278cf51282a1be8fd671b5f2ce1028550596d
nehaspt <129504897+nehaspt@users.noreply.github.com>
2023-04-21 15:49:19 +0530

    Update hello.java

--- hello.java
+++ hello.java
@@ -1,6 +1,6 @@
 class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, World welcome to spt!");
-	 System.out.println("Hello welcome to java world");
+	 System.out.println("Hello welcome ");
     }
 }
